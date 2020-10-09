7698b49b82e41f060325051905bd93185ad064fd
abulaiz <ronabulaiz@gmail.com>
2020-10-09 08:55:10 +0700

    Add Vehicle Controller

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Vehicle;
+
+class VehicleController extends Controller
+{
+    public function index(Request $request){
+
+    }
+
+    public function store(Request $request){
+
+    }
+}
