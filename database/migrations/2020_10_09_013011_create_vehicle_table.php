35a5273a491cc0e66c21aa1971c41404a984fcdb
abulaiz <ronabulaiz@gmail.com>
2020-10-09 08:47:48 +0700

    Create Vehicle Table Migration

---
+++ database/migrations/2020_10_09_013011_create_vehicle_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('plat_number');
+            $table->string('name');
+            $table->float('engine_displacement');
+            $table->string('engine_displacement_unit');
+            $table->integer('engine_power');
+            $table->integer('price');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle');
+    }
+}
