1539e59f611b1575b0cc9e1234b4e0673b1b187a
abulaiz <ronabulaiz@gmail.com>
2020-10-09 20:11:20 +0700

    Add Store API for store new vehicle

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -7,11 +7,56 @@ use Illuminate\Support\Facades\Validator;
 
 class VehicleController extends Controller
 {
+	private $accepted_engine_displacement_unit = ['liters', 'centimeters', 'inches'];
+
+	private $accepted_engine_power_unit = ['hoursepower', 'kilowatts'];
+
+	private $engine_displacement_unit_alias = [
+		'liters' => 'L', 'centimeters' => 'cm', 'inches' => 'inches'
+	];
+
+	private $engine_power_unit_alias = [
+		'hoursepower' => 'HP', 'kilowatts' => 'kW'
+	];
+
     public function index(Request $request){
 
     }
 
+    /*
+	 * Store new vehicel to Database
+     */
     public function store(Request $request){
+    	$input = $request->all();
 
+        $validator = Validator::make($input, [
+        	'plat_number' => 'required|unique:vehicels,plat_number',
+        	'name' => 'required',
+        	'engine_displacement' => 'required|numeric',
+        	'engine_displacement_unit' => 'required|in:'.implode(',', $this->accepted_engine_displacement_unit),
+        	'engine_power' => 'required',
+        	'engine_power_unit' => 'required|in:'.implode(',', $this->accepted_engine_power_unit),
+        	'price' => 'required|numeric',
+        	'location' => 'required'
+        ]);   
+
+        if ($validator->fails())
+            return response()->json([
+            	'errors' => $validator->errors()->getMessages(),
+            	'success' => false
+            ]);  
+
+        DB::beginTransaction();
+
+        try {
+        	$input['created_at'] = date('Y-m-d H:i:s');
+        	DB::table('vehicels')->insert($input);
+        	DB::commit();
+		} catch (\Exception $e) {
+			DB::rollback();
+			return response()->json(['success' => false]);  
+		}      
+
+		return response()->json(['success' => true]); 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'api'], function () use ($router) {
+	$router->group(['prefix' => 'v1'], function () use ($router) {
+		$router->post('vehicle', 'VehicleController@store');
+	});
 });
